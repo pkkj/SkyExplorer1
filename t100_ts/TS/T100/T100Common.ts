a505c062541f96d6a96b333ae82581291cb623e3
pkkj <gz.pkkj@gmail.com>
2015-07-10 23:21:28 -0700

    T100: support data to 2014.12

--- t100_ts/TS/T100/T100Common.ts
+++ t100_ts/TS/T100/T100Common.ts
@@ -12,12 +12,12 @@
                 this.supportAirportReportPage = true;
                 this.country = "United States";
                 this.startTime = new YearMonth(1990, 1);
-                this.endTime = new YearMonth(2014, 9);
+                this.endTime = new YearMonth(2014, 12);
             }
             static currentCountry = "United States";
             static hasMonthData = true;
             static dataFrom = new YearMonth(1990, 1);
-            static dataTo = new YearMonth(2014, 9);
+            static dataTo = new YearMonth(2014, 12);
 
             static availablity: string[] = ["2014", "2013", "2012", "2011", "2010", "2009", "2008", "2007", "2006", "2005", "2004", "2003", "2002", "2001", "2000", "1999",
                 "1998", "1997", "1996", "1995", "1994", "1993", "1992", "1991", "1990"];
